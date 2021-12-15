673e44a8860fcec7c079993ed24f1ae7fcd44a63
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2021-12-15 15:00:01 +0700

    player die

--- src/logic/HomeScreen.java
+++ src/logic/HomeScreen.java
@@ -9,6 +9,8 @@ import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.effect.Glow;
+import javafx.scene.effect.Shadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
@@ -20,6 +22,7 @@ public class HomeScreen extends Pane {
 
 	private Label name1;
 	private Label name2;
+	private Label name3;
 	private Button start;
 	private Button quit;
 	
@@ -28,18 +31,22 @@ public class HomeScreen extends Pane {
 		setPrefSize(1280, 720);
 		name1 = new Label("ZENITH");
 		name2 = new Label("CHRONICLE");
+		name3 = new Label("III");
 		start = new Button("Start");
 		quit = new Button("Quit");
 		
-		name1.setStyle("-fx-text-fill: white;-fx-font-style: italic; -fx-font-size: 120px;-fx-font-weight: bold; -fx-font-family: \"Tahoma\";");
-		name2.setStyle("-fx-text-fill: white;-fx-font-style: italic; -fx-font-size: 120px;-fx-font-weight: bold; -fx-font-family: \"Tahoma\";");
+		name1.setStyle("-fx-text-fill: gray;-fx-font-style: italic; -fx-font-size: 128px;-fx-font-weight: bold; -fx-font-family: \"Roboto\";");
+		name2.setStyle("-fx-text-fill: white;-fx-font-style: italic; -fx-font-size: 128px;-fx-font-weight: bold; -fx-font-family: \"Roboto\";");
+		name3.setStyle("-fx-text-fill: white;-fx-font-style: italic; -fx-font-size: 148px;-fx-font-weight: bold; -fx-font-family: \"Roboto\";");
 		
-		name1.setLayoutX(60);name2.setLayoutY(90);
-		name2.setLayoutX(400);name2.setLayoutY(150);
+		name1.setLayoutX(70);name2.setLayoutY(90);
+//		name1.setEffect(new Glow());
+		name2.setLayoutX(300);name2.setLayoutY(120);
+//		Shadow s = new Shadow();s.setColor(Color.PINK);s.setRadius(2);s.setWidth(2);
+//		name2.setEffect(s);
+		name3.setLayoutX(1030);name3.setLayoutY(100);
 		start.setLayoutX(120);start.setLayoutY(500);
 		quit.setLayoutX(120);quit.setLayoutY(600);
-		
-		start.setBorder(null);
 		
 		start.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
@@ -47,14 +54,13 @@ public class HomeScreen extends Pane {
 				GameManager.getInstance().gameStart(e);
 			}
 		});
-		
 		quit.setOnAction(new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e) {
 				stage.close();
 			}
 		});
 		
-		getChildren().addAll(name1,name2,start,quit);
+		getChildren().addAll(name1,name2,name3,start,quit);
 		
 	}
 

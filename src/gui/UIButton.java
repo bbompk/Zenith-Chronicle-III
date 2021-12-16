3d651ffc52250062b6776fac046b6a8bf7c16075
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2021-12-16 20:21:22 +0700

    doc template

--- src/gui/UIButton.java
+++ src/gui/UIButton.java
@@ -127,12 +127,23 @@ public class UIButton extends Button {
 		});
 	}
 	
-	public UIButton(String t,int x,int y,int fontSize,Color color,boolean transparent,int insetstype) {
+	public UIButton(String t,int x,int y,int fontSize,Color color,boolean transparent) {
 		super(t);
 		setLayoutX(x);setLayoutY(y);
 		setFont(FontHolder.getInstance().getFont().get(fontSize));
 		setTextFill(color);
-		if(transparent)setStyle("-fx-border-color: transparent;-fx-border-width: 0;-fx-background-radius: 0; -fx-background-color: transparent;");
+		if(transparent) {
+			setPadding(new Insets(0));
+			setStyle("-fx-border-color: transparent;-fx-border-width: 0;-fx-background-radius: 0; -fx-background-color: transparent;");
+		}
+		else if(color.equals(Color.BLACK)) {
+			setPadding(new Insets(4,0,4,0));
+			setStyle("-fx-border-color: transparent;-fx-border-width: 0;-fx-background-radius: 20; -fx-background-color: white;");
+		}
+		else {
+			setPadding(new Insets(8,12,8,12));
+			setStyle("-fx-border-color: transparent;-fx-border-width: 0;-fx-background-radius: 20; -fx-background-color: black;");
+		}
 		
 	}
 
